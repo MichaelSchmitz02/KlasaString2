482a275480701661e401b483a071a3d07ba6374e
MichaelSchmitz02 <152979941+MichaelSchmitz02@users.noreply.github.com>
2024-01-16 12:10:28 +0100

    Add files via upload

---
+++ Vjezba Klasa String 2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Unesite Niz Znakova: ");
+        string unos = Console.ReadLine();
+        string rezultat = ZamijeniRazmake(unos);
+        Console.WriteLine("Rezultat Zamjene Razmaka: " + rezultat);
+    }
+    static string ZamijeniRazmake(string ulaz)
+    {
+        return ulaz.Replace(' ', '_');
+    }
+}
+
